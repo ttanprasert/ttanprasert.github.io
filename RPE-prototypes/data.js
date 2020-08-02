15110e5c3d4d1d28992201928f8011860ec7553e
ttanprasert <ttanprasert@hmc.edu>
2020-08-02 01:56:08 -0700

    Add files via upload

---
+++ RPE-prototypes/data.js
@@ -0,0 +1,5 @@
+var area1 = {x1:92, y1:70, x2:142, y2:88, startTime:4, endTime:8};
+var area2 = {x1:143, y1:70, x2:198, y2:88, startTime:8, endTime:10};
+var area3 = {x1:198, y1:70, x2:240, y2:88, startTime:10, endTime:12};
+
+var areaList = {area1, area2, area3};
