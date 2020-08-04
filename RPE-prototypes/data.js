39fddf28569bdd3f4217ff9b2531739c798c6ceb
ttanprasert <ttanprasert@hmc.edu>
2020-08-04 06:28:07 -0700

    Add files via upload

--- RPE-prototypes/data.js
+++ RPE-prototypes/data.js
@@ -1,5 +1,69 @@
-var area1 = {x1:92, y1:70, x2:142, y2:88, startTime:4, endTime:8};
-var area2 = {x1:143, y1:70, x2:198, y2:88, startTime:8, endTime:10};
-var area3 = {x1:198, y1:70, x2:240, y2:88, startTime:10, endTime:12};
+var area00 = {x1:132, y1:80, x2:139, y2:95, startTime:16, endTime:17, color:'c'};
+var area01 = {x1:141, y1:80, x2:147, y2:95, startTime:17, endTime:18, color:'c'};
+var area02 = {x1:148, y1:80, x2:155, y2:95, startTime:17, endTime:19, color:'c'};
+var area10 = {x1:161, y1:80, x2:170, y2:132, startTime:18, endTime:21, color:'c'};
+var area11 = {x1:180, y1:80, x2:187, y2:95, startTime:20, endTime:21, color:'c'};
+var area12 = {x1:187, y1:80, x2:195, y2:95, startTime:20, endTime:22, color:'c'};
+var area13 = {x1:198, y1:80, x2:203, y2:95, startTime:22, endTime:23, color:'c'};
+var area14 = {x1:206, y1:80, x2:212, y2:95, startTime:22, endTime:24, color:'c'};
+var area15 = {x1:214, y1:80, x2:220, y2:95, startTime:23, endTime:24, color:'c'};
+var area20 = {x1:229, y1:80, x2:235, y2:132, startTime:24, endTime:25, color:'c'};
+var area21 = {x1:236, y1:80, x2:244, y2:95, startTime:24, endTime:26, color:'c'};
+var area22 = {x1:245, y1:80, x2:252, y2:95, startTime:25, endTime:28, color:'c'};
+var area23 = {x1:268, y1:80, x2:273, y2:95, startTime:28, endTime:29, color:'c'};
+var area24 = {x1:275, y1:80, x2:280, y2:95, startTime:29, endTime:30, color:'c'};
+var area25 = {x1:284, y1:80, x2:288, y2:95, startTime:29, endTime:31, color:'c'};
+var area30 = {x1:297, y1:80, x2:302, y2:132, startTime:30, endTime:33, color:'c'};
+var area31 = {x1:314, y1:80, x2:318, y2:95, startTime:32, endTime:34, color:'c'};
+var area32 = {x1:321, y1:80, x2:326, y2:95, startTime:33, endTime:35, color:'c'};
+var area33 = {x1:332, y1:80, x2:336, y2:95, startTime:34, endTime:35, color:'c'};
+var area34 = {x1:340, y1:80, x2:344, y2:95, startTime:35, endTime:36, color:'c'};
+var area35 = {x1:349, y1:80, x2:352, y2:95, startTime:35, endTime:37, color:'c'};
+var area40 = {x1:69, y1:185, x2:74, y2:237, startTime:36, endTime:38, color:'c'};
+var area41 = {x1:81, y1:222, x2:86, y2:237, startTime:38, endTime:40, color:'c'};
+var area42 = {x1:93, y1:222, x2:98, y2:237, startTime:39, endTime:41, color:'c'};
+var area43 = {x1:100, y1:185, x2:105, y2:200, startTime:40, endTime:41, color:'c'};
+var area44 = {x1:107, y1:185, x2:113, y2:237, startTime:40, endTime:42, color:'c'};
+var area45 = {x1:115, y1:185, x2:119, y2:200, startTime:41, endTime:43, color:'c'};
+var area50 = {x1:127, y1:185, x2:133, y2:237, startTime:42, endTime:45, color:'c'};
+var area51 = {x1:143, y1:185, x2:149, y2:200, startTime:44, endTime:46, color:'c'};
+var area52 = {x1:152, y1:185, x2:157, y2:200, startTime:45, endTime:47, color:'c'};
+var area53 = {x1:159, y1:185, x2:164, y2:200, startTime:46, endTime:47, color:'c'};
+var area54 = {x1:167, y1:185, x2:171, y2:200, startTime:47, endTime:48, color:'c'};
+var area55 = {x1:174, y1:185, x2:179, y2:200, startTime:47, endTime:49, color:'c'};
+var area60 = {x1:186, y1:185, x2:192, y2:237, startTime:48, endTime:50, color:'c'};
+var area61 = {x1:194, y1:185, x2:199, y2:200, startTime:49, endTime:50, color:'c'};
+var area62 = {x1:202, y1:185, x2:206, y2:200, startTime:50, endTime:52, color:'c'};
+var area63 = {x1:213, y1:222, x2:218, y2:200, startTime:51, endTime:53, color:'c'};
+var area64 = {x1:220, y1:185, x2:225, y2:200, startTime:52, endTime:53, color:'c'};
+var area65 = {x1:227, y1:185, x2:232, y2:200, startTime:53, endTime:54, color:'c'};
+var area66 = {x1:234, y1:185, x2:239, y2:200, startTime:53, endTime:55, color:'c'};
+var area70 = {x1:246, y1:185, x2:252, y2:237, startTime:54, endTime:57, color:'c'};
+var area71 = {x1:262, y1:185, x2:267, y2:200, startTime:56, endTime:58, color:'c'};
+var area72 = {x1:269, y1:185, x2:274, y2:237, startTime:57, endTime:59, color:'c'};
+var area73 = {x1:276, y1:185, x2:282, y2:200, startTime:58, endTime:59, color:'c'};
+var area74 = {x1:285, y1:185, x2:290, y2:200, startTime:59, endTime:60, color:'c'};
+var area75 = {x1:291, y1:185, x2:297, y2:200, startTime:59, endTime:61, color:'c'};
+var area80 = {x1:302, y1:185, x2:309, y2:237, startTime:60, endTime:62, color:'c'};
+var area81 = {x1:314, y1:222, x2:319, y2:237, startTime:62, endTime:64, color:'c'};
+var area82 = {x1:326, y1:222, x2:332, y2:237, startTime:63, endTime:65, color:'c'};
+var area83 = {x1:333, y1:185, x2:338, y2:200, startTime:64, endTime:65, color:'c'};
+var area84 = {x1:342, y1:185, x2:346, y2:200, startTime:65, endTime:66, color:'c'};
+var area85 = {x1:346, y1:185, x2:353, y2:200, startTime:65, endTime:67, color:'c'};
 
-var areaList = {area1, area2, area3};
+var areaList = [area00, area01, area02, area10, area11, area12, area13, area14, area15, area20, area21, area22, area23, area24, area25,
+                area30, area31, area32, area33, area34, area35, area40, area41, area42, area43, area44, area45, area50, area51, area52,
+                area53, area54, area55, area60, area61, area62, area63, area64, area65, area66, area70, area71, area72, area73, area74,
+                area75, area80, area81, area82, area83, area84, area85];
+
+function getArea(i) {
+    return areaList[i];
+};
+
+function changeAreaColor(i, c) {
+    areaList[i].color = c;
+};
+
+function getNumArea(){
+    return areaList.length;
+};
